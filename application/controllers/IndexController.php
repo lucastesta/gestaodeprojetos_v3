ef0a7344b8fee08742c37252cbacf0d4f6c66835
lucas <lucas@lucas-pc>
2013-10-10 13:23:20 -0300

    Versão 1 - 10/10/2013 - 13:23 Form Cadastra

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,6 +11,40 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         $this->view->pagina = 'cadastra';
+        
+        
+        $form = new Application_Form_Cadastrar();
+        $this->view->status = $form->setStatusSelect('status');
+        $this->view->cliente = $form->setText('cliente', 35);
+        $this->view->tituloProjeto = $form->setText('tituloProjeto', 55);
+        $this->view->subProjetoFAI = $form->setText('subProjetoFAI', 35);
+        $this->view->unidade = $form->setUnidadeSelect('unidade');
+        $this->view->resumo = $form->setTextArea('resumo', 12, 50);
+        $this->view->origem = $form->setTextArea('origem', 12, 50);
+        $this->view->dataAprovacao = $form->setText('dataAprovacao', 25,true,  array('class' => 'data'));
+        $this->view->duracaoProjeto = $form->setText('duracaoProjeto', 25,true,  array('onkeypress' => 'return SomenteNumero(event)'));
+        $this->view->dataPrevistaIni = $form->setText('dataPrevistaIni', 25,true, array('class' => 'data'));
+        $this->view->dataPrevistaTer = $form->setText('dataPrevistaTer', 25, true, array('class' => 'data'));
+        $this->view->valorProposto = $form->setText('valorProposto', 20, true, array('class' => 'money'));
+        $this->view->ProjetoServico = $form->setRadioServico('categoria');
+        $this->view->ob = $form->setTextArea('ob', 12, 55);
+        $this->view->submit = $form->setSubmit('cadastrar');
+        
+        $form->addElements(array($this->view->cliente
+                            ,$this->view->status,
+                            $this->view->tituloProjeto,
+                            $this->view->subProjetoFAI,
+                            $this->view->unidade,
+                            $this->view->resumo,
+                            $this->view->origem,
+                            $this->view->dataAprovacao,
+                            $this->view->duracaoProjeto,
+                            $this->view->dataPrevistaIni,
+                            $this->view->dataPrevistaTer,
+                            $this->view->valorProposto,
+                            $this->view->ProjetoServico,
+                            $this->view->ob,
+                            $this->view->submit));
     }
 
     public function alterarAction()
---
+++ application/forms/Cadastrar.php
@@ -0,0 +1,83 @@
+<?php
+
+class Application_Form_Cadastrar extends Zend_Form
+{
+
+    public function init()
+    {
+        
+    }
+    
+    
+    public function setStatusSelect($nome) {
+        $status = new Zend_Form_Element_Select($nome);
+        $status->setMultiOptions(array(
+            '1' => 'Em Prospecção',
+            '2' => 'Em Andamento',
+            '3' => 'Recusado',
+            '4' => 'Finalizado'
+        ))->addValidator(new Zend_Validate_Int(), false);
+        return $status;
+    }
+    
+    public function setText($name, $size = 18, $add = false, $op = array()) {
+        $texto = new Zend_Form_Element_Text($name);
+        $texto->setRequired(true)
+                ->addFilter('StripTags')
+                ->addFilter('StringTrim')
+                ->setAttrib('size', $size)
+                ->addValidator('NotEmpty')
+                ->addErrorMessage('Valor não pode ser vazio');
+        if(!$add)
+            return $texto;
+        if($add) {
+            foreach($op as $k => $v) {
+                $texto->setAttrib($k, $v);
+            }
+            return $texto;
+        }
+    }
+    
+    public function setUnidadeSelect($nome){
+        $unidade = new Zend_Form_Element_Select($nome);
+        $unidade->setMultiOptions(array(
+            '1' => 'UN Combustíveis',
+            '2' => 'UN Metais/Cerâmicas',
+            '3' => 'UN Polímeros',
+            '4' => 'UN Sorocaba',
+            '5' => 'Corporate'
+        ))->addValidator(new Zend_Validate_Int(), false);
+        return $unidade;
+    }
+    
+    public function setTextArea($nome, $row, $col) {
+        $area = new Zend_Form_Element_Textarea($nome);
+        $area->setRequired(true)
+             ->addValidator('NotEmpty')
+                ->addFilter('StripTags')
+                ->addFilter('StringTrim')
+                ->addErrorMessage('Escreva uma mensagem');
+        $area->setAttrib('rows', $row);
+        $area->setAttrib('cols', $col);
+        return $area;
+    }
+    
+    public function setRadioServico($name)  {
+        $campo = new Zend_Form_Element_Radio($name);
+        $campo->addMultiOptions(array(
+            '1' => 'Projeto',
+            '2' => 'Serviço Tecnológico'
+        ))->setValue('1');
+        return $campo;
+    }
+    
+    public function setSubmit($name) {
+        $campo = new Zend_Form_Element_Submit($name);
+        $campo->setValue('Cadastrar');
+        return $campo;
+    }
+
+
+
+}
+

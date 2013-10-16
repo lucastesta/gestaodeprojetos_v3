6f1eb919eacfa8fa106a227569899b49f3080da0
lucas <lucas@lucas-pc>
2013-10-16 19:23:18 -0300

    Colocando Investimento Previsto

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -8,7 +8,7 @@ function SomenteNumero(e){
 
 $(document).ready(function() {
     $('input[name=Alterar]').click(function() {
-        jAlert('mensagem', 'Alert Dialog');
+        jAlert('success', 'mensagem', 'Alert Dialog');
     });
 });
 
